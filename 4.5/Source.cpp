e3998ddb73445bca0f00dbda3c012643249c6820
torrros <vladtoros96@gmail.com>
2021-10-13 22:11:04 +0300

    Add project files.

---
+++ 4.5/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R= "; cin >> R;
+		if (((y >= 0) &&
+			(pow(R, 2) >= pow(y, 2) + pow(x + R, 2)) &&
+			(x >= -R))
+			|| ((y <= 0) && (pow(x - R, 2) >= pow(y, 2) + pow(x - R, 2)) && (x <= R)))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+		for (int i = 0; i < 10; i++)
+		{
+			x = 2. * rand() / RAND_MAX - R;
+			y = 2. * rand() / RAND_MAX - R;
+			if (((y >= 0) &&
+				(pow(R, 2) >= pow(y, 2) + pow(x + R, 2)) &&
+				(x >= -R))
+				|| ((y <= 0) && (pow(x - R, 2) >= pow(y, 2) + pow(x - R, 2)) && (x <= R)))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) <<(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
